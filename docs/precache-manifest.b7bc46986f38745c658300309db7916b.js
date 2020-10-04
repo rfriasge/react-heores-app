b22bf5141bbe84da93bc23980f06cf37494d3552
rafa <rfriasge@gmail.com>
2020-10-04 08:30:27 -0400

    Docs agregado

---
+++ docs/precache-manifest.b7bc46986f38745c658300309db7916b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cb62827d8fce1b303290787525ab908",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b3af7bb4a7049c64efe",
+    "url": "/static/js/2.57201cb6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.57201cb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d89188f6d07ae371bfd9",
+    "url": "/static/js/main.7901efa9.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
